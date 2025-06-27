3ef018e838ebd47a7088f70621a0f1a979aa6b80
Fksecurity <12345678+yourusername@users.noreply.github.com>
2025-06-27 22:22:52 +0900

    🔧 디버깅

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
